86dd5ae10518d114fcb31ce73291a948c72968cf
admin <admin@admindeiMac-2.local>
2015-07-27 16:40:40 +0800

    add favicon

--- honvue_pag/urls.py
+++ honvue_pag/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = patterns('company.views', 
     url(r'^$', 'index'),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
 )
