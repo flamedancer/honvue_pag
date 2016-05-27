7c13397cd2c9788faf957c9261b7e4887218b121
admin <admin@admindeiMac-2.local>
2016-05-27 17:57:55 +0800

    adjust templates

--- company/configs.py
+++ company/configs.py
@@ -31,10 +31,19 @@ GAMES = [
         'bigpicture': "/static/images/bannerTD.png",
         'smpicture': "/static/images/smallTD.png",
         'name': u'泰迪爱消除',
-        'showpictures': ["/static/images/smallTD.png","/static/images/smallTD.png","/static/images/smallTD.png"],
-        'small_desc': u"休闲三消,6月9日，与电影同步上线!",
-        'main_desc': "我是游戏简介111111111",
-        'feature_desc': "我是游戏特色11111111",
+        'showpictures': ["/static/images/pic_xuanchuan1.png","/static/images/pic_xuanchuan2.png","/static/images/pic_xuanchuan5.png"],
+        'small_desc': u"休闲三消,泰迪正版授权",
+        'main_desc': u'''
+    《泰迪爱消除》是由泰迪熊正版授权的一款三消类休闲游戏。游戏紧贴《泰迪熊之玩具大战》电影剧情，通过控制三个主角泰迪、小飞和罗巴，一起粉碎巫博士通过控制机甲玩具征服世界的阴谋，并以爽快流畅的消除手感，绚丽缤纷的消除效果，独创的微策略闯关玩法，趣味与挑战并存的关卡，突出主角特色的养成系统，加上清新诱人的美食风格以及泰迪熊电影原版音乐和配音，带给玩家高品质的动感消除体验。''',
+        'feature_desc':u''' 
+《泰迪熊》正版电影授权
+    《泰迪爱消除》由电影《泰迪熊之玩具大战》官方正版授权，游戏剧情紧贴电影，并使用电影原版音乐和配音，完美还原泰迪熊和小伙伴之间的友情、热血和正义。
+		
+看的见的“美味”
+    作为一款轻松休闲的三消游戏，本作采用了可爱的卡通风格画面，并将游戏中的各种基础珠和小孩子最喜欢的美味零食相结合，隔着屏幕都能闻到那美食的香甜气息。
+	
+创新消除玩法 角色养成系统 战斗系统
+    游戏在传统的三消基础上，引入了角色养成玩法和战斗系统，让整个游戏过程更加丰富多彩。除了泰迪、玩家还可以培养如小飞或者罗巴，每个主角都有不同的属性加成以及技能效果，这让每一关都能有不同的通关方法，让玩家感受最完整的泰迪熊世界。''',
         'download_tags': [ ["/static/images/apple.png", "/static/images/apple1.png", "IOS", ""], ["/static/images/android.png", "/static/images/android1.png", u"安卓", ""]],
         'type_tags': [u'休闲', u'三消'],
     },
@@ -117,7 +126,7 @@ JOBS = [
 2、与程序美术协同工作，保证开发结果符合设计要求；
 3、熟悉Excel表格，完成Excel配置；
 4、利用CocoStudio编辑器完成界面设计；
-5、协助完成测试工作
+5、协助完成测试工作  
 
 
 任职要求：
